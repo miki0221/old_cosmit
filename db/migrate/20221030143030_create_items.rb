66930ce36cbdae7f0b864e1c021e216b422338a5
miki0221 <miki.shibutani0221@gmail.com>
2022-10-31 23:42:29 +0900

    itemsモデル作成

---
+++ db/migrate/20221030143030_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string  :title,       null: false
+      t.text    :content,     null: false
+      t.integer :category_id, null: false
+      t.date    :start_date,  null: false
+      t.timestamps
+    end
+  end
+end
