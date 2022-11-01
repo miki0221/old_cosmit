eaaafb6fe6952de859d7bb3069f6fe0aa07bb2c4
miki0221 <miki.shibutani0221@gmail.com>
2022-11-02 00:33:36 +0900

    カテゴリー系モデルの作成

---
+++ app/models/eye.rb
@@ -0,0 +1,12 @@
+class Eye < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'アイシャドウ' },
+    { id: 3, name: 'アイライナー' },
+    { id: 4, name: 'マスカラ' },
+    { id: 5, name: 'アイブロウ'},
+    { id: 6, name: 'その他'}
+  ]
+  include ActiveHash::Associations
+  has_many :items
+  end
---
+++ app/models/lip.rb
@@ -0,0 +1,13 @@
+class Lip < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'リップスティック' },
+    { id: 3, name: 'リキッドリップ' },
+    { id: 4, name: 'リップグロス' },
+    { id: 5, name: 'リップティント'},
+    { id: 6, name: 'リップペンシル'},
+    { id: 7, name: 'その他'}
+  ]
+  include ActiveHash::Associations
+  has_many :items
+  end
