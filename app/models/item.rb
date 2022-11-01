0d852bd5fd5f4e7b1ffacb83838b9ca9fa6908ad
miki0221 <miki.shibutani0221@gmail.com>
2022-11-02 00:33:19 +0900

    itemsモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :face
+  belongs_to :eye
+  belongs_to :lip
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image, blob: { content_type: :image }
+    validates :title, :content, :start_date
+    validates :category_id, :face_id, :eye_id, :lip_id, numericality: { other_than: 1, message: "can't be blank" } 
+  end
 end
