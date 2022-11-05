647ec5d5298e412e3572037c72646d96bf19d3ba
miki0221 <miki.shibutani0221@gmail.com>
2022-11-06 00:13:31 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :face
-  belongs_to :eye
-  belongs_to :lip
-  belongs_to :user
+  belongs_to :category_option
   has_one_attached :image
 
   with_options presence: true do
     validates :image, blob: { content_type: :image }
     validates :title, :content, :start_date
-    validates :category_id, :category_option, numericality: { other_than: 1, message: "can't be blank" }
+    validates :category_id, :category_option_id, numericality: { other_than: 1, message: "can't be blank" }
   end
 end
